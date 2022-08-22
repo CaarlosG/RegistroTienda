67d68bc64405de56067c21df2341b26454fe2ba7
CaarlosG <104378210+CaarlosG@users.noreply.github.com>
2022-08-22 08:51:31 -0600

    Conexión a base de datos
    Conexión de base de datos a el programa en visual basic

--- ProyectoTienda/ProyectoTienda/Inicio_sesion.vb
+++ ProyectoTienda/ProyectoTienda/Inicio_sesion.vb
@@ -1,4 +1,5 @@
-﻿Public Class Inicio_sesion
+﻿Imports MySql.Data.MySqlClient
+Public Class Inicio_sesion
     Private Sub Inicio_sesion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
